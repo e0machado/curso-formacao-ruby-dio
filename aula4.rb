00eb1d1ab64f54e9824a151c50d547604ff24ba3
Evandro Machado <evandromachado@outlook.com>
2024-01-13 17:54:27 -0300

    Adicionado projeto 1

---
+++ aula4.rb
@@ -0,0 +1,12 @@
+# 1)Saída de dados na tela --> aparecer um comando na tela
+# usuário digite seu nome
+print "Digite seu nome: "
+
+# 2)Programa permita que usuário preencha o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+# 3) Saída final
+# para chamar um dado #{variável}
+puts "Oi #{nome}!"
+
---
+++ projetoNomeIdade.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Usuário #{nome} #{sobrenome} tem #{idade} anos."
